e777debb5daf49052a5d71fbf832766e993a11c4
hasbi <ashshidiq23@gmail.com>
2023-02-11 00:16:08 +0700

    feat: implementing bottom navbar

--- app/src/main/java/com/unikom/payup/ui/main/MainActivity.kt
+++ app/src/main/java/com/unikom/payup/ui/main/MainActivity.kt
@@ -3,10 +3,13 @@ package com.unikom.payup.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.unikom.payup.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavView.background = null
+        bottomNavView.menu.getItem(2).isEnabled = false
     }
 }
